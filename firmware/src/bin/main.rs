3bd6fe41d2a95b61c4c0ee61a8b6403690f09dcc
Astrick <astrick.harren.at.work@gmail.com>
2025-05-16 22:45:52 +0800

    refactor: fix i2s settings

--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -4,6 +4,7 @@
 
 use core::str::FromStr;
 
+use bytes::BytesMut;
 use embassy_executor::Spawner;
 use embassy_net::udp::PacketMetadata;
 use embassy_net::udp::UdpSocket;
@@ -17,6 +18,7 @@ use esp_hal::timer::timg::TimerGroup;
 use esp_hal::{dma_circular_buffers_chunk_size, Async};
 use firmware::wifi::{WifiConfig, WifiConnection};
 use log::{debug, error, info, warn, LevelFilter};
+use opus::Decoder;
 
 #[esp_hal_embassy::main]
 async fn main(s: Spawner) {
@@ -54,7 +56,7 @@ async fn main(s: Spawner) {
             esp_hal::i2s::master::Standard::Philips,
             esp_hal::i2s::master::DataFormat::Data16Channel16,
             Rate::from_khz(16),
-            peripherals.DMA_I2S0,
+            peripherals.DMA_I2S1,
             rx_d,
             tx_d,
         )
@@ -99,8 +101,8 @@ async fn udp_play(stack: Stack<'_>, i2s_tx: I2sTx<'_, Async>, rx_buf: &mut [u8])
 
     info!("Waiting for udp packets");
     let buf = &mut [0; 1024];
-    let _pcm = &mut [0; 2048];
-    // let mut dec = Decoder::new(16000, opus::Channels::Mono).unwrap();
+    let pcm = &mut [0; 2048];
+    let mut dec = Decoder::new(16000, opus::Channels::Mono).unwrap();
     let mut transfer = i2s_tx.write_dma_circular_async(rx_buf).unwrap();
     loop {
         match udp
@@ -111,12 +113,13 @@ async fn udp_play(stack: Stack<'_>, i2s_tx: I2sTx<'_, Async>, rx_buf: &mut [u8])
             Ok((n, _)) if n > 0 => {
                 debug!("Udp recved {n} bytes");
                 let buf = &mut buf[..n];
-                //let n = dec.decode(buf, pcm, false).unwrap();
-                //let mut pcm: BytesMut = pcm
-                //    .into_iter()
-                //    .take(n * 2)
-                //    .flat_map(|x| x.to_le_bytes())
-                //    .collect();
+                let n = dec.decode(buf, pcm, false).unwrap();
+                let mut pcm: BytesMut = pcm
+                    .into_iter()
+                    .take(n)
+                    .flat_map(|x| [*x, *x])
+                    .flat_map(|x| x.to_le_bytes())
+                    .collect();
 
                 transfer
                     .push(buf)
